9c29ffd99eac0bae345070a46047a1b2fc0a8ebb
Drew Engberson <drew.engberson@gmail.com>
2018-10-07 17:26:00 -0600

    Write Course model tests.

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,6 @@
 class Enrollment < ApplicationRecord
   belongs_to :section
   belongs_to :student
+
+  validates_presence_of :section, :student
 end
--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @c = Course.new
+  end
+
+  test 'must_have_name' do
+    @c.number = "390p"
+    @c.credits = 1
+    @c.department = "CS"
+
+    assert_throws(@c, "exception") {throw @c}
+  end
+
+  test 'must_have_number' do
+    @c.name = "Web Apps"
+    @c.credits = 1
+    @c.department = "CS"
+
+    assert_throws(@c, "exception") {throw @c}
+  end
+
+  test 'must_have_credits' do
+    @c = Course.new
+    @c.name = "Web Apps"
+    @c.number = "390p"
+    @c.department = "CS"
+
+    assert_throws(@c, "exception") {throw @c}
+  end
+
+  test 'must_have_department' do
+    @c = Course.new
+    @c.name = "Web Apps"
+    @c.number = "390p"
+    @c.credits = 1
+
+    assert_throws(@c, "exception") {throw @c}
+  end
 end
